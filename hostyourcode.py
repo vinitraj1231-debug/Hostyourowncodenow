f5c4b9446ab54489f05dea0619e30c868e120934
vinitraj1231-debug <vinitraj1231@gmail.com>
2026-02-04 22:53:39 +0530

    Update hostyourcode.py

--- hostyourcode.py
+++ hostyourcode.py
@@ -2765,9 +2765,7 @@ def login():
         if user.get('is_banned'):
             return redirect('/login?error=Account banned. Contact support')
         
-         if user['device_fingerprint'] != fingerprint:
-            return redirect('/login?error=Security error. Please use your original device')
-        
+         
         
         # Update last login
         update_user(user_id, last_login=datetime.now().isoformat())
